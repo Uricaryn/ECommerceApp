1d33faa5d53bc5903a757a6dc2484583efeecc34
Uricaryn <onuranatca@hotmail.com.tr>
2024-09-21 19:02:14 +0300

    Add project files.

---
+++ Core/ECommerceAPI.Application/Features/Commands/Products/CreateProduct/CreateProductCommandRequest.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceAPI.Application.DTOs.Prodcuts;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features.Commands.Products.CreateProduct
+{
+    public class CreateProductCommandRequest : IRequest<CreateProductCommandResponse>
+    {
+        public CreateProductDTO Product { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
